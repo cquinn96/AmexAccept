2187b6743d5523f656c0ae26f85de850d7b69331
Cormac Quinn <cquinn96@qub.ac.uk>
2015-07-14 11:03:58 +0100

    Added API Key. Added Google Play Services Dependencies.

--- app/src/main/java/com/hackathon/amex/amexaccept/MainActivity.java
+++ app/src/main/java/com/hackathon/amex/amexaccept/MainActivity.java
@@ -1,12 +1,66 @@
 package com.hackathon.amex.amexaccept;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+//    private GoogleApiClient mGoogleApiClient;
+//
+//    private TextView mViewName = (TextView) findViewById(R.id.mViewName);
+//    private TextView mViewAddress = (TextView) findViewById(R.id.mViewName);
+//    private TextView mViewAttributions = (TextView) findViewById(R.id.mViewName);
+//
+//    public void onPickButtonClick(View v) {
+//        // Construct an intent for the place picker
+//        try {
+//            PlacePicker.IntentBuilder intentBuilder =
+//                    new PlacePicker.IntentBuilder();
+//            Intent intent = intentBuilder.build(this);
+//            // Start the intent by requesting a result,
+//            // identified by a request code.
+//            startActivityForResult(intent, REQUEST_PLACE_PICKER);
+//
+//        } catch (GooglePlayServicesRepairableException e) {
+//            // ...
+//        } catch (GooglePlayServicesNotAvailableException e) {
+//            // ...
+//        }
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode,
+//                                    int resultCode, Intent data) {
+//
+//        if (requestCode == REQUEST_PLACE_PICKER
+//                && resultCode == Activity.RESULT_OK) {
+//
+//            // The user has selected a place. Extract the name and address.
+//            final Place place = PlacePicker.getPlace(data, this);
+//
+//            final CharSequence name = place.getName();
+//            final CharSequence address = place.getAddress();
+//            String attributions = PlacePicker.getAttributions(data);
+//            if (attributions == null) {
+//                attributions = "";
+//            }
+//
+//            mViewName.setText(name);
+//            mViewAddress.setText(address);
+//            mViewAttributions.setText(Html.fromHtml(attributions));
+//
+//        } else {
+//            super.onActivityResult(requestCode, resultCode, data);
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
